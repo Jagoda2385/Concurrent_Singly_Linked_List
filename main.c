1c7f898e976c24be3bf20f1e04d6df198a890922
Jagoda Wesołowska <155394422+Jagoda2385@users.noreply.github.com>
2025-01-23 23:09:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 #include "list.h"
 
-
 int main() {
-    printf("Tworzenie listy o rozmiarze (5)\n");
-    TList *list = createList(5);
-    
-    int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6;
-    
-    printf("Dodawanie elementow \n");
-    putItem(list, &a);
-    putItem(list, &b);
-    putItem(list, &c);
-    putItem(list, &d);
-    putItem(list, &e);
-    showList(list);
-    printf("Aktualna liczba elementów: %d\n", getCount(list));
-    
+    // Tworzenie listy o maksymalnym rozmiarze 5
+    TList* list = createList(5);
+    if (!list) {
+        printf("Nie udało się utworzyć listy.\n");
+        return 1;
+    }
 
-    printf("Usuwanie najstarszego elementu z listy\n");
-    void *item = getItem(list);
-    printf("Retrieved item: %d\n", *(int *)item);
+    // Dodawanie elementów do listy
+    char *items[] = {"A", "B", "C", "D", "E"};
+    for (int i = 0; i < 5; i++) {
+        char *item = strdup(items[i]);
+        if (!item) {
+            printf("Błąd alokacji pamięci\n");
+            return 1;
+        }
+        putItem(list, item);
+        printf("Dodano: %s\n", item);
+    }
     showList(list);
-    printf("Aktualna liczba elementów: %d\n", getCount(list));
-    
-    printf("Usuwanie z listy elementu (3)\n");
-    removeItem(list, &c);
+
+    // Pobieranie elementów z listy
+    for (int i = 0; i < 2; i++) {
+        char *retrieved = (char*)getItem(list);
+        if (retrieved) {
+            printf("Pobrano: %s\n", retrieved);
+            free(retrieved);
+        }
+    }
     showList(list);
+
+    // Usuwanie elementu z listy
+    char toRemove[] = "D";
+    Node *current = list->head;
+    while (current) {
+        if (strcmp((char*)current->data, toRemove) == 0) {
+            printf("Usuwanie elementu: %d\n", removeItem(list, current->data));
+            break;
+        }
+        current = current->next;
+    }
+    showList(list);
+
+    // Sprawdzanie liczby elementów na liście
     printf("Aktualna liczba elementów: %d\n", getCount(list));
-    
-    printf("Ustalanie nowego maxymalnego rozmiaru listy (3)\n");
-    setMaxSize(list, 3);
-    
-    printf("tworzenie list2 i przenoszenie wszytskich elementow list do list2\n");
-    TList *list2 = createList(3);
-    int g = 7, h = 8;
-    putItem(list2, &g);
-    putItem(list2, &h);
+
+    // Zmiana maksymalnego rozmiaru listy
+    setMaxSize(list, 10);
+    printf("Nowy maksymalny rozmiar listy ustawiony.\n");
+
+    // Tworzenie drugiej listy i dodawanie elementów
+    TList* list2 = createList(5);
+    if (!list2) {
+        printf("Nie udało się utworzyć drugiej listy.\n");
+        return 1;
+    }
+    char *itemX = strdup("X");
+    char *itemY = strdup("Y");
+    if (!itemX || !itemY) {
+        printf("Błąd alokacji pamięci\n");
+        return 1;
+    }
+    putItem(list2, itemX);
+    putItem(list2, itemY);
+    printf("Lista 2 przed przeniesieniem:\n");
+    showList(list2);
+
+    // Przenoszenie elementów z listy2 do listy
     appendItems(list, list2);
+    printf("Lista po dodaniu elementów z listy2:\n");
     showList(list);
-    printf("Aktualna liczba elementów: %d\n", getCount(list));
-     
-    printf("Usuwanie list2\n");
-    destroyList(list2);
 
-    printf("Dodawanie kolejnego elementu do list, operacja blokująca \n");
-    putItem(list, &f);
-    showList(list);
-    
+    // Usuwanie listy
+    destroyList(list);
+    destroyList(list2);
+    printf("Listy zostały usunięte.\n");
+
     return 0;
 }
