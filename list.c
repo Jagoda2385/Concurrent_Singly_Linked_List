f6b5874dfea63bf1984546990f41687aea5e74f8
Jagoda Wesołowska <155394422+Jagoda2385@users.noreply.github.com>
2025-07-08 11:02:44 +0200

    Update list.c

--- list.c
+++ list.c
@@ -4,11 +4,13 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+// Definition of a singly linked list node
 typedef struct Node {
     void *data;
     struct Node *next;
 } Node;
 
+// Definition of the concurrent singly linked list
 typedef struct {
     Node *first_element;
     Node *last_element;
@@ -22,34 +24,38 @@ typedef struct {
     pthread_cond_t no_active_threads;
 } TList;
 
+// Creates a new list with a given maximum size
 TList* createList(int ListSize) {
     TList *list = (TList*)malloc(sizeof(TList));
     if (!list) return NULL;
+
     list->first_element = list->last_element = NULL;
     list->num_of_elements = 0;
     list->maxSize = ListSize;
     list->destroy = 0;
     list->active_threads = 0;
+
     pthread_mutex_init(&list->lock, NULL);
     pthread_cond_init(&list->not_empty, NULL);
     pthread_cond_init(&list->not_full, NULL);
     pthread_cond_init(&list->no_active_threads, NULL);
+
     return list;
 }
 
+// Frees all resources and destroys the list
 void destroyList(TList* list) {
     pthread_mutex_lock(&list->lock);
 
-    list->destroy = 1; 
-    pthread_cond_broadcast(&list->not_empty);  
-    pthread_cond_broadcast(&list->not_full);   
-    pthread_cond_broadcast(&list->no_active_threads);  
+    list->destroy = 1;
+    pthread_cond_broadcast(&list->not_empty);
+    pthread_cond_broadcast(&list->not_full);
+    pthread_cond_broadcast(&list->no_active_threads);
 
-    while (list->active_threads > 0) {  
+    while (list->active_threads > 0) {
         pthread_cond_wait(&list->no_active_threads, &list->lock);
     }
 
-    
     Node *current = list->first_element;
     while (current) {
         Node *next = current->next;
@@ -62,25 +68,26 @@ void destroyList(TList* list) {
     list->num_of_elements = 0;
 
     pthread_mutex_unlock(&list->lock);
-  
+
     pthread_mutex_destroy(&list->lock);
     pthread_cond_destroy(&list->not_empty);
     pthread_cond_destroy(&list->not_full);
     pthread_cond_destroy(&list->no_active_threads);
-    
+
     free(list);
 }
 
+// Adds a new item to the list (blocks if list is full)
 void putItem(TList* list, void *el) {
     pthread_mutex_lock(&list->lock);
 
-    if (list->destroy) {  
+    if (list->destroy) {
         pthread_mutex_unlock(&list->lock);
         return;
     }
 
     while (list->num_of_elements >= list->maxSize) {
-        if (list->destroy) { 
+        if (list->destroy) {
             pthread_mutex_unlock(&list->lock);
             return;
         }
@@ -92,6 +99,7 @@ void putItem(TList* list, void *el) {
         pthread_mutex_unlock(&list->lock);
         return;
     }
+
     newNode->data = el;
     newNode->next = NULL;
 
@@ -107,16 +115,17 @@ void putItem(TList* list, void *el) {
     pthread_mutex_unlock(&list->lock);
 }
 
+// Retrieves and removes the first item from the list (blocks if empty)
 void* getItem(TList* list) {
     pthread_mutex_lock(&list->lock);
 
-    if (list->destroy) { 
+    if (list->destroy) {
         pthread_mutex_unlock(&list->lock);
         return NULL;
     }
 
     while (list->num_of_elements == 0) {
-        if (list->destroy) { 
+        if (list->destroy) {
             pthread_mutex_unlock(&list->lock);
             return NULL;
         }
@@ -126,9 +135,11 @@ void* getItem(TList* list) {
     Node *oldNode = list->first_element;
     void *data = oldNode->data;
     list->first_element = oldNode->next;
+
     if (!list->first_element) {
         list->last_element = NULL;
     }
+
     free(oldNode);
     list->num_of_elements--;
 
@@ -137,16 +148,17 @@ void* getItem(TList* list) {
     return data;
 }
 
+// Retrieves and removes the last item from the list (blocks if empty)
 void* popItem(TList* list) {
     pthread_mutex_lock(&list->lock);
-    
+
     if (list->destroy) {
         pthread_mutex_unlock(&list->lock);
         return NULL;
     }
 
     while (list->num_of_elements == 0) {
-        if (list->destroy) { 
+        if (list->destroy) {
             pthread_mutex_unlock(&list->lock);
             return NULL;
         }
@@ -160,6 +172,7 @@ void* popItem(TList* list) {
     }
 
     void *data = current->data;
+
     if (prev) {
         prev->next = NULL;
         list->last_element = prev;
@@ -175,6 +188,7 @@ void* popItem(TList* list) {
     return data;
 }
 
+// Removes a specific item from the list
 int removeItem(TList* list, void *el) {
     pthread_mutex_lock(&list->lock);
 
@@ -196,9 +210,11 @@ int removeItem(TList* list, void *el) {
             } else {
                 list->first_element = current->next;
             }
+
             if (!current->next) {
                 list->last_element = prev;
             }
+
             free(current->data);
             free(current);
             list->num_of_elements--;
@@ -216,10 +232,11 @@ int removeItem(TList* list, void *el) {
     return 0;
 }
 
+// Returns the current number of elements in the list
 int getCount(TList* list) {
     pthread_mutex_lock(&list->lock);
 
-    if (list->destroy) { 
+    if (list->destroy) {
         pthread_mutex_unlock(&list->lock);
         return 0;
     }
@@ -229,38 +246,39 @@ int getCount(TList* list) {
     return num_of_elements;
 }
 
+// Sets a new maximum size for the list
 void setMaxSize(TList* list, int n) {
     pthread_mutex_lock(&list->lock);
 
-    if (list->destroy) { 
+    if (list->destroy) {
         pthread_mutex_unlock(&list->lock);
         return;
     }
 
     list->maxSize = n;
-    pthread_cond_broadcast(&list->not_full);  
+    pthread_cond_broadcast(&list->not_full);
 
     pthread_mutex_unlock(&list->lock);
 }
 
+// Appends all items from list2 to list1
 void appendItems(TList* list, TList* list2) {
     pthread_mutex_lock(&list->lock);
     pthread_mutex_lock(&list2->lock);
 
-    if (list->destroy || list2->destroy) { 
+    if (list->destroy || list2->destroy) {
         pthread_mutex_unlock(&list2->lock);
         pthread_mutex_unlock(&list->lock);
         return;
     }
 
     if (list2->first_element) {
-
         if (list->last_element) {
             list->last_element->next = list2->first_element;
         } else {
             list->first_element = list2->first_element;
         }
-        
+
         list->last_element = list2->last_element;
         list->num_of_elements += list2->num_of_elements;
 
@@ -275,12 +293,11 @@ void appendItems(TList* list, TList* list2) {
     pthread_mutex_unlock(&list->lock);
 }
 
-
-
+// Prints the contents of the list
 void showList(TList* list) {
     pthread_mutex_lock(&list->lock);
     Node *current = list->first_element;
-    printf("Lista: ");
+    printf("List: ");
     while (current) {
         printf("\"%s\" ", (char*)current->data);
         current = current->next;
